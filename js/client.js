f1eb7f03edac7dfc2bb4ee7ba38848bd2cabd3d9
MoldyHacker <immoldy@gmail.com>
2023-02-01 21:14:56 -0600

    Module 2 Grid and Toast

--- js/client.js
+++ js/client.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function () {
     // preload audio
     var toast = new Audio('media/toast.wav');
 
-    $('.code').on('click', function(e) {
+    $('.code').on('click', function (e) {
         e.preventDefault();
         // first pause the audio (in case it is still playing)
         toast.pause();
@@ -10,6 +10,15 @@ $(function(){
         toast.currentTime = 0;
         // play audio
         toast.play();
+
+        $('.toast-header #product').text($(this).attr('data-product'));
+        $('.toast-body #code').text($(this).attr('data-code'));
         $('#toast').toast({ autohide: false }).toast('show');
     });
+});
+
+$(document).on('keydown', function (event) {
+    if (event.key == "Escape") {
+        $('#toast').toast('hide')
+    }
 });
